890be9019b57fdbb3ba0a672d3cdaffedd368fe6
Spartak-Sargsyan <spartak.sargsyan.77@gmail.com>
2023-12-06 23:11:15 +0400

    feat: create context and update roiting

--- src/routes/AuthenticatedApp.tsx
+++ src/routes/AuthenticatedApp.tsx
@@ -2,23 +2,29 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import ProfilePage from "../components/organizm/Profile Page/ProfilePage";
 import { AuthenticatedRoutePath } from "../constant/routes";
 import Task from "../components/template/Register/UserTask/Task";
+import Layout from "../components/organizm/Layout/Layout";
 
 const AuthenticatedApp = createBrowserRouter([
   {
-    element: <ProfilePage />,
-    path: AuthenticatedRoutePath.Profile(),
-    errorElement: <div>Error happend</div>,
-    loader: () => <div>Loading</div>,
-  },
-  {
-    element: <Task />,
-    path: AuthenticatedRoutePath.Tasks(),
-    errorElement: <div>Error happend</div>,
-    loader: () => <div>Loading</div>,
-  },
-  {
-    path: AuthenticatedRoutePath.Navigate(),
-    element: <Navigate to={AuthenticatedRoutePath.Profile()} />,
+    element: <Layout />,
+    children: [
+      {
+        element: <ProfilePage />,
+        path: AuthenticatedRoutePath.Profile(),
+        errorElement: <div>Error happend</div>,
+        loader: () => <div>Loading</div>,
+      },
+      {
+        element: <Task />,
+        path: AuthenticatedRoutePath.Tasks(),
+        errorElement: <div>Error happend</div>,
+        loader: () => <div>Loading</div>,
+      },
+      {
+        path: AuthenticatedRoutePath.Navigate(),
+        element: <Navigate to={AuthenticatedRoutePath.Profile()} />,
+      },
+    ],
   },
 ]);
 
